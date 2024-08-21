33de7ec5a210d25365bea3efa8a056d26449c417
Admin <Admin@supriyavijay>
2024-08-21 09:25:47 +0530

    pushing

---
+++ Demowebshoprajeshwari/src/test/java/BooksPage/ToCreateBooPage.java
@@ -0,0 +1,10 @@
+package BooksPage;
+
+public class ToCreateBooPage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Books page added successfully");
+	}
+
+}
---
+++ Demowebshoprajeshwari/src/test/java/ComputersPage/ToCrateComputersPage.java
@@ -0,0 +1,10 @@
+package ComputersPage;
+
+public class ToCrateComputersPage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Computers Page Added Sucessfully");
+	}
+
+}
